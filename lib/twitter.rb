766714a1ebc92568631a9d3e614691d9ec32a96f
Jason Haruska <jason@haruska.com>
2008-07-02 16:44:40 -0400

    improve twitter api lib

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -1,27 +1,69 @@
 module Twitter
+
+  SUCCESS = :success
+  UNAUTHORIZED = :unauthorized
+  UNKNOWN = :unknown
   
   # Updates the authenticating user's status.
-  # Requires the status parameter specified below.  
+  # Requires the status parameter specified below.
   # Request must be a POST.
-  # 
+  #
   # URL: http://twitter.com/statuses/update.format
-  # Formats: xml, json.  Returns the posted status in requested format when successful.
+  # Formats: xml, json. Returns the posted status in requested format when successful.
   # Parameters:
   #
-  # status.  Required.  The text of your status update.  
-  # Be sure to URL encode as necessary.  Must not be more than 160 characters and should 
+  # status. Required. The text of your status update.
+  # Be sure to URL encode as necessary. Must not be more than 160 characters and should
   # not be more than 140 characters to ensure optimal display.
   
   def self.update_status(name, pass, status)
-    url = URI.parse("http://#{CGI::escape(name)}:#{CGI::escape(pass)}@twitter.com/statuses/update.xml")
-    res = Net::HTTP.post_form(url, {'status' => status, 'source' => 'fb2twit'})
-    case res.code_type.to_s
-    when "Net::HTTPOK"
-      :success
-    when "Net::HTTPUnauthorized"
-      :unauthorized
+    url = URI.parse("http://twitter.com/statuses/update.xml")
+    req = Net::HTTP::Post.new url.path
+    req.basic_auth name, pass
+    
+    req.set_form_data 'status' => status, 'source' => 'fb2twit'
+    
+    net = Net::HTTP.new url.host
+    net.open_timeout = 30
+    net.read_timeout = 30
+    
+    begin
+      res = net.start { |http| http.request req }
+    rescue Timeout::Error
+      return Twitter::UNKNOWN
+    end 
+    
+    self.response_value res
+  end
+  
+  
+  def self.auth_check(name, pass)
+    url = URI.parse("http://twitter.com/statuses/friends_timeline.json")
+    req = Net::HTTP::Get.new url.path
+    req.basic_auth name, pass
+    
+    net = Net::HTTP.new url.host
+    net.read_timeout = 15
+    
+    begin
+      res = net.start { |http| http.request req }
+    rescue Timeout::Error
+      return Twitter::UNKNOWN
+    end
+    
+    self.response_value res
+  end
+  
+  protected
+  
+  def self.response_value(http_response)
+    case http_response
+    when Net::HTTPOK
+      Twitter::SUCCESS
+    when Net::HTTPUnauthorized
+      Twitter::UNAUTHORIZED
     else
-      :unknown
+      Twitter::UNKNOWN
     end
   end
   
